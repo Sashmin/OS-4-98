bc7b17db0c066fce6e04342b63220cf471639d69
Sashmin <tolochko.minsk@gmail.com>
2022-12-23 15:31:12 +0300

    simple read and write interface

--- Reciever/Receiver.cpp
+++ Reciever/Receiver.cpp
@@ -1,23 +1,59 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <iostream>
+#include <conio.h>
 
 const int MAX_BINFILE_NAME = 50;
+const int NAME_SIZE = 30;
+const int MAX_MESSAGE_SIZE = 20;
 
 int main()
 {
 	char binFileName[MAX_BINFILE_NAME] = "";
-	int maxRecords;
-	int numOfSenders;
+	char lpCommandLineTemplate[100] = "Sender.exe";
+	char stEvNameTemp[NAME_SIZE] = "";
+	const char* mutexName = "accessMutex";
+	const char* readSemName = "readSem";
+	const char* writeSemName = "writeSem";
+	int maxRecords = 0;
+	int numOfSenders = 0;
 
-	printf("Enter the name of the binary file: ");
+	printf("Enter the name of the binary file (without extension): ");
 	scanf_s("%s", binFileName, MAX_BINFILE_NAME);
+	strcat_s(binFileName, ".bin");
 
 	printf("Enter the maximum number of records in the file: ");
 	scanf_s("%d", &maxRecords);
 	
 	printf("Enter the number of Sender processes: ");
 	scanf_s("%d", &numOfSenders);
+
+	strcat_s(lpCommandLineTemplate, " ");
+	strcat_s(lpCommandLineTemplate, binFileName);
+
+	FILE* fin;
+	errno_t err_fin = fopen_s(&fin, binFileName, "w+b");
+	fclose(fin);
+
+	HANDLE hMutex = CreateMutexA(
+		nullptr,
+		true,
+		mutexName
+	);
+
+	HANDLE hReadSemaphore = CreateSemaphoreA(
+		nullptr,
+		0,
+		maxRecords,
+		readSemName
+	);
+
+	HANDLE hWriteSemaphore = CreateSemaphoreA(
+		nullptr,
+		maxRecords,
+		maxRecords,
+		writeSemName
+	);
 
 	HANDLE* hSenders = new HANDLE[numOfSenders];
 	HANDLE* hStartEvents = new HANDLE[numOfSenders];
@@ -30,8 +66,6 @@ int main()
 		ZeroMemory(&pi[i], sizeof pi);
 	}
 
-
-	char stEvNameTemp[30] = "";
 	for (int i = 0; i < numOfSenders; i++) {
 		sprintf_s(stEvNameTemp, "Start %d", i);
 		hStartEvents[i] = CreateEventA(
@@ -41,10 +75,6 @@ int main()
 			stEvNameTemp
 		);
 	}
-
-	char lpCommandLineTemplate[100] = "Sender.exe";
-	strcat_s(lpCommandLineTemplate, " ");
-	strcat_s(lpCommandLineTemplate, binFileName);
 
 	char lpCommandLine[100];
 	for (int i = 0; i < numOfSenders; i++) {
@@ -67,5 +97,31 @@ int main()
 
 	WaitForMultipleObjects(numOfSenders, hStartEvents, true, 5000);
 
+	char choice = 'r';
+	char message[MAX_MESSAGE_SIZE + 1] = "";
+	int currPosition = 0;
+
 	
+	while (choice == 'r') {
+		printf("To read a message, press R key \nTo exit, press any other key      ");
+		choice = _getch();
+		printf("%c\n", choice);
+
+		if (choice == 'r') 
+		{
+			err_fin = fopen_s(&fin, binFileName, "rb");
+			if (ftell(fin) != SEEK_END)
+			{
+				fseek(fin, 0, currPosition);
+				fread(message, MAX_MESSAGE_SIZE, sizeof(char), fin);
+				printf("Message: %s \n", message);
+				currPosition += MAX_MESSAGE_SIZE;
+			}
+			else
+			{
+				printf("End of file\n");
+			}
+			fclose(fin);
+		}
+	}
 }
--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -2,15 +2,42 @@
 #include <Windows.h>
 #include <conio.h>
 
+const int MAX_MESSAGE_SIZE = 20;
+
 int main(int argc, char* argv[])
 {
+	const char* mutexName = "accessMutex";
+	const char* readSemName = "readSem";
+	const char* writeSemName = "writeSem";
+	const char* binFileName = argv[1];
 	int id = atoi(argv[2]);
+	
+
 	char stEvName[30] = "";
 	sprintf_s(stEvName, "Start %s", argv[2]);
+
 	HANDLE hStartEv = OpenEventA(
 		EVENT_ALL_ACCESS,
 		true,
 		stEvName
+	);
+
+	HANDLE hMutex = OpenMutexA(
+		MUTEX_ALL_ACCESS,
+		true,
+		mutexName
+	);
+
+	HANDLE hreadSemaphore = OpenSemaphoreA(
+		SEMAPHORE_ALL_ACCESS,
+		true,
+		readSemName
+	);
+
+	HANDLE hwriteSemaphore = OpenSemaphoreA(
+		SEMAPHORE_ALL_ACCESS,
+		true,
+		writeSemName
 	);
 	
 	if (hStartEv) {
@@ -19,6 +46,33 @@ int main(int argc, char* argv[])
 	else {
 		printf("error opening startEvent");
 	}
+
+	char choice = 'r';
+	char message[MAX_MESSAGE_SIZE + 1] = "";
+	int currPosition = 0;
+	FILE* fin;
+	errno_t err_fin;
+
+
+	while (choice == 'r') {
+		printf("To read a message, press R key \nTo exit, press any other key      ");
+		choice = _getch();
+		printf("%c\n", choice);
+
+		if (choice == 'r')
+		{
+			err_fin = fopen_s(&fin, binFileName, "ab");
+
+			printf("Enter message: ");
+			scanf_s("%s", message, MAX_MESSAGE_SIZE);
+
+			fwrite(message, MAX_MESSAGE_SIZE, sizeof(char), fin);
+			fclose(fin);
+			
+		}
+	}
+
+
 	
 	_getch();
 }
